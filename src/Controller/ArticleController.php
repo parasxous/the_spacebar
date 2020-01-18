433a366f81a06e894a1c408276473c659b5f2e46
Parasxous <parasxous@MacBook-Pro-Parasxous.local>
2020-01-18 15:07:45 +0200

    familiriaze with symfony

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,23 +3,43 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 
-
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('omg my first page');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name= "article_show")
      */
     public function show($slug){
-        return new Response(sprintf(" news " .$slug));
+
+    $comments =[
+        'Now would this be apple wood smoked bacon? Or traditional bacon - IMHO it makes a difference.',
+        'Now would this be apple wood smoked bacon? Or traditional bacon - IMHO it makes a difference.',
+        'Now would this be apple wood smoked bacon? Or traditional bacon - IMHO it makes a difference.'
+    ];
+        return $this->render('article/show.html.twig',[
+            'title'=>ucwords(str_replace('-',' ',$slug)),
+            'slug'=>$slug,
+            'comments'=>$comments
+            ]);
+    }
+    /**
+     * @Route("/news/{slug}/heart", name = "article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger){
+        $logger->info("article is being hearted");
+        //todo actualy heart/unheart the article
+        return new JsonResponse(['hearts'=>rand(5,100)]);
     }
 }
